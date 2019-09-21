8a46501933392532f27775f597ebbc0dc3205181
Tariod <mykhailovdmt@gmail.com>
2019-09-21 10:44:18 +0300

    Fix union infinite recursion

--- course_03/multi_paradigm_programming/set/union.lisp
+++ course_03/multi_paradigm_programming/set/union.lisp
@@ -1,5 +1,6 @@
 (defun my_union (set1 set2)
   (cond
+    ((null set1) set2)
     ((null set2) set1)
     ((null (member (car set1) set2))
       (append (list (car set1)) (my_union (cdr set1) set2)))
