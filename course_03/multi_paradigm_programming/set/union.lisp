901fdd1678a3ed8e98ee22b5fa6f96ff3127a906
Tariod <mykhailovdmt@gmail.com>
2019-09-21 09:02:54 +0300

    Refactor my_union according to union contract

--- course_03/multi_paradigm_programming/set/union.lisp
+++ course_03/multi_paradigm_programming/set/union.lisp
@@ -1,8 +1,8 @@
 (defun my_union (set1 set2)
   (cond
     ((null set2) set1)
-    ((null (member (car set2) set1))
-      (append (my_union set1 (cdr set2)) (list (car set2))))
-    (T (my_union set1 (cdr set2)))
+    ((null (member (car set1) set2))
+      (append (list (car set1)) (my_union (cdr set1) set2)))
+    (T (my_union (cdr set1) set2))
   )
 )
