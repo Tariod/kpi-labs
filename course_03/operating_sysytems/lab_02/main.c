1bba92057f0b93ceaebd3e5addef694b0345c1fa
Tariod <mykhailovdmt@gmail.com>
2019-10-21 17:35:14 +0300

    feat(OS): implement mem_alloc function

--- course_03/operating_sysytems/lab_02/main.c
+++ course_03/operating_sysytems/lab_02/main.c
@@ -1,7 +1,10 @@
 #include "src/segregated_list.h"
+#include <stdio.h>
 
 int main (int argc, char *argv[]) {
   mem_pool_alloc();
+  mem_alloc(32);
+  mem_alloc(16);
   mem_pool_free();
   return 0;
 }
--- course_03/operating_sysytems/lab_02/src/segregated_list.c
+++ course_03/operating_sysytems/lab_02/src/segregated_list.c
@@ -14,6 +14,13 @@ static size_t align(size_t size);
 static pages_pool_t *pages_pool_alloc();
 static page_t *page_alloc();
 static block_t *block_alloc();
+static pages_pool_t *get_pages_pool(size_t block_size);
+static pages_pool_t *get_empty_pages_pool();
+static block_t *get_empty_block(page_t *page);
+static pages_pool_t *unshift_pages_pool(pages_pool_t *pages_pool);
+static page_t *segregate_page(page_t *page, size_t size);
+static page_t *shift_page(pages_pool_t *pages_pool);
+static page_t *unshift_page(pages_pool_t *pages_pool, page_t *page);
 
 // Implementation of public functions
 
@@ -99,6 +106,45 @@ void mem_pool_free(void) {
   }
 };
 
+void *mem_alloc(size_t size) {
+  size_t block_size = align(size);
+  pages_pool_t *pages_pool = get_pages_pool(block_size);
+  if (pages_pool == NULL) {
+    pages_pool_t *empty_pages_pool = get_empty_pages_pool();
+    pages_pool = pages_pool_alloc();
+
+    if (pages_pool == NULL) {
+      return pages_pool;
+    }
+  
+    pages_pool->size = block_size;
+    unshift_pages_pool(pages_pool);
+  }
+
+  page_t *page = pages_pool->pages;
+  block_t *empty_block = get_empty_block(page);
+  while (page != NULL && page->next != NULL && empty_block == NULL) {
+    page = page->next;
+    empty_block = get_empty_block(page);
+  }
+
+  if (empty_block == NULL) {
+    pages_pool_t *empty_pages_pool = get_empty_pages_pool();
+
+    page = shift_page(empty_pages_pool);
+    if (page == NULL) {
+      return page;
+    }
+    page = segregate_page(page, block_size);
+    unshift_page(pages_pool, page);
+
+    empty_block = page->blocks->payload;
+  }
+
+  empty_block->free = false;
+  return empty_block->payload;
+};
+
 // Implementation of private functions
 
 static size_t align(size_t size) {
@@ -143,4 +189,85 @@ static block_t *block_alloc() {
   block->next = NULL;
 
   return block;
+};
+
+static pages_pool_t *get_pages_pool(size_t block_size) {
+  pages_pool_t *pages_pool = heap.pages_pool;
+  while (block_size != pages_pool->size) {
+    pages_pool = pages_pool->next;
+    if (pages_pool == NULL) {
+      return NULL;
+    }
+  }
+
+  return pages_pool;
+};
+
+static pages_pool_t *get_empty_pages_pool() {
+  return get_pages_pool(0);
+};
+
+static block_t *get_empty_block(page_t *page) {
+  if (page == NULL) {
+    return NULL;
+  }
+
+  block_t *block = page->blocks;
+  while (!block->free) {
+    block = block->next;
+    if (block == NULL) {
+      return NULL;
+    }
+  }
+
+  return block;
+};
+
+static pages_pool_t *unshift_pages_pool(pages_pool_t *pages_pool) {
+  pages_pool_t *next = heap.pages_pool;
+
+  pages_pool->next = next;
+  heap.pages_pool = pages_pool;
+
+  return pages_pool;
+};
+
+static page_t *segregate_page(page_t *page, size_t size) {
+  block_t *block = page->blocks;
+  void *payload = (void *)((size_t)block->payload + size);
+  void *tail = (void *)((size_t)payload + PAGE_SIZE);
+
+  while (payload != tail) {
+    block->next = block_alloc();
+    block = block->next;
+    block->payload = payload;
+    payload = (void *)((size_t)payload + size); 
+  }
+
+  return page;
+};
+
+static page_t *shift_page(pages_pool_t *pages_pool) {
+  page_t *page = pages_pool->pages;
+  if (page != NULL) {
+    pages_pool->pages = pages_pool->pages->next;
+    if (pages_pool->pages != NULL) {
+      pages_pool->pages->prev = NULL;
+    }
+    page->next = NULL;
+  }
+
+  return page;
+};
+
+static page_t *unshift_page(pages_pool_t *pages_pool, page_t *page) {
+  page_t *next = pages_pool->pages;
+
+  page->next = next;
+  if (next != NULL) {
+    next->prev = page;
+  }
+  pages_pool->pages = page;
+
+  return page;
 };
